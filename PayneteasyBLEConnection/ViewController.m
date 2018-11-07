515beb3a479b072315800f05eb1d4b4f944876d8
Sergey Anisiforov <sa@payneteasy.com>
2018-11-07 15:23:57 +0300

    Added scanning peripherals

--- PayneteasyBLEConnection/ViewController.m
+++ PayneteasyBLEConnection/ViewController.m
@@ -2,16 +2,21 @@
 #import "ViewController.h"
 #import <CoreBluetooth/CoreBluetooth.h>
 
+#define Object(obj) obj ? obj : [NSNull null]
+
 @interface ViewController () <UITextFieldDelegate, CBCentralManagerDelegate>
 
 @property (nonatomic, weak) IBOutlet UITextField *textField;
 @property (nonatomic, weak) IBOutlet UITextView *textView;
+@property (nonatomic, weak) IBOutlet UIButton *buttonScan;
 
 @end
 
-@implementation ViewController {
-    CBCentralManager *centralManager;
 
+@implementation ViewController {
+    NSMutableArray *_arrayIdentifiers;
+    NSMutableDictionary *_dictPeripherals;
+    CBCentralManager *_centralManager;
 }
 
 - (void)viewDidLoad {
@@ -19,10 +24,54 @@
     
     _textView.text = nil;
     
-    centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
+    _arrayIdentifiers = [NSMutableArray array];
+    _dictPeripherals = [NSMutableDictionary dictionary];
+    _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
 
+- (void)startScan {
+    _textView.text = nil;
+    CBUUID *serviceUUID = [CBUUID UUIDWithString:_textField.text];
+    if (serviceUUID) {
+        [_centralManager scanForPeripheralsWithServices:nil options:nil];
+        [_buttonScan setTitle:@"Stop scanning" forState:UIControlStateNormal];
+    }
+}
+
+- (void)stopScan {
+    [_centralManager stopScan];
+    [_buttonScan setTitle:@"Start scanning" forState:UIControlStateNormal];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self retrieveConnectedPeripherals];
+    return YES;
+}
+
+#pragma mark - CBCentralManagerDelegate
+
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI {
+    NSString *identifier = peripheral.identifier.UUIDString;
+    if (identifier) {
+        if (![_arrayIdentifiers containsObject:identifier])
+            [_arrayIdentifiers addObject:identifier];
+        _dictPeripherals[identifier] = @{ @"UUID" : identifier, @"name" : Object(peripheral.name), @"advertisementData" : Object(advertisementData)};
+    }
+    NSInteger num = 0;
+    NSString *text = nil;
+    for (NSString *identifier in _arrayIdentifiers) {
+        num++;
+        NSDictionary *dict = _dictPeripherals[identifier];
+        NSString *item = [NSString stringWithFormat:@"%ld. UUID=%@\nname=%@\nadvertisementData=%@", (long)num, dict[@"UUID"], dict[@"name"], dict[@"advertisementData"]];
+        text = text.length ? [text stringByAppendingFormat:@"\n\n%@", item] : item;
+    }
+    _textView.text = text;
+    
 }
 
 - (void)retrieveConnectedPeripherals {
@@ -31,17 +80,25 @@
     
     CBUUID *serviceUUID = [CBUUID UUIDWithString:_textField.text];
     if (serviceUUID) {
-        NSArray<CBPeripheral *> *array = [centralManager retrieveConnectedPeripheralsWithServices:@[serviceUUID]];
+        NSArray<CBPeripheral *> *array = [_centralManager retrieveConnectedPeripheralsWithServices:@[serviceUUID]];
         _textView.text = [array description];
     }
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [self retrieveConnectedPeripherals];
-    return YES;
+#pragma mark - Actions
+
+- (IBAction)buttonScanTouchUpInside:(UIButton *)button {
+    if (_centralManager.isScanning) {
+        [self stopScan];
+    } else {
+        [self startScan];
+    }
 }
 
-- (IBAction)buttonTouchUpInside:(id)sender {
+- (IBAction)buttonRetrieveTouchUpInside:(UIButton *)button {
+    if (_centralManager.isScanning) {
+        [self stopScan];
+    }
     [self retrieveConnectedPeripherals];
 }
 
